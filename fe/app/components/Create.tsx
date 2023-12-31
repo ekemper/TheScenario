d6d06d37b2f0fb0347492a1923e8982fbe4f4189
Edward Kemper <edwardkemper@gmail.com>
2023-12-31 12:20:55 -0700

    working form

--- fe/app/components/Create.tsx
+++ fe/app/components/Create.tsx
@@ -1,29 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 import { Formik, Field, Form } from "formik";
-import { Button } from "@nextui-org/react";
 import { FC } from 'react';
 
 interface CreateProps {
     setIsLoading: any
 }
 
-const Create: FC<CreateProps> = ({setIsLoading}) => {
+const Create: FC<CreateProps> = ({ setIsLoading }) => {
     return (
         <>
-            <Button color="primary" variant="bordered" className="justify-end h-auto w-auto p-2">
-                <div className="flex row  justify-between">
-                    <FontAwesomeIcon className="h-10" icon={faCirclePlus} />
-                    <h2 className="uppercase ">Create</h2>
-                </div>
-            </Button>
-
-
-        {/* TODO: Accessibility for form! */}
-
+            {/* TODO: Accessibility for form! */}
             <div>
                 <Formik
-                    initialValues={{ name: "", email: "" }}
+                    initialValues={{ data: "" }}
                     onSubmit={async (values) => {
                         setIsLoading(true)
                         await new Promise((resolve) => setTimeout(resolve, 500));
@@ -31,10 +21,16 @@ const Create: FC<CreateProps> = ({setIsLoading}) => {
                         alert(JSON.stringify(values, null, 2));
                     }}
                 >
-                    <Form>
-                        <Field name="name" type="text" />
-                        <Field name="email" type="email" />
-                        <button type="submit">Submit</button>
+                    <Form className='flex'>
+                        <Field 
+                            className="h-12 mr-4 p-2 rounded-xl" 
+                            name="data" 
+                            type="text" 
+                            placeholder="new datum"/>
+
+                        <button type="submit" className="rounded-full ">
+                            <FontAwesomeIcon className="h-10 " icon={faCirclePlus} />
+                        </button>
                     </Form>
                 </Formik>
             </div>
